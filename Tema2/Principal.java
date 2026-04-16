d7130846fb29cbd04949108c81eda02b9d2ed9c3
LRP <lufer5775@gmail.com>
2026-04-16 17:38:18 +0200

    Limpiar repo: quitar del control de versiones lo ignorado por gitignore

--- Tema2/Principal.java
+++
@@ -1,7 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        ClasePruebas clase = new ClasePruebas();
-        double valor = clase.echaCuentas();
-        System.out.println("Sale " + valor);
-    }
-}
